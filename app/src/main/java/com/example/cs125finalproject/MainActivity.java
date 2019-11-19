c6efa0dc6ed7433f2edbaae7a8b090356bcbeac1
nicscharpman <ns23@illinois.edu>
2019-11-19 11:24:03 -0600

    Made some BS changes

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cs125finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button destroyChallen = findViewById(R.id.destroy);
+        Button reincarnate = findViewById(R.id.reincarnate);
     }
 }
