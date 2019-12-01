6b95c5cac2fb1f892a28167482d8458dfacbaf9c
zujjainwala <zakirlu2@illinois.edu>
2019-11-30 18:43:42 -0600

    Merge branch 'master' of https://github.com/nscharpman/CS-125-Final-Project
    # Conflicts:
    #	app/src/main/java/com/example/cs125finalproject/GameActivity.java

--- app/src/main/java/com/example/cs125finalproject/GameActivity.java
+++ app/src/main/java/com/example/cs125finalproject/GameActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class GameActivity extends AppCompatActivity {
     /** Background view to change at each event */
     private ImageView view = findViewById(R.id.background);
+    // We need to set the image view for each function that we define. Therefore need multiple picture options.
 
     /** Button for the first action a player could make */
     private Button actionOne = findViewById(R.id.actionOne);
@@ -23,28 +24,70 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
+        text.setText("Stuff and Things");
 
         actionOne.setOnClickListener(unused -> firstEvent());
         actionTwo.setOnClickListener(unused -> secondEvent());
-
-        text.setText("Stuff and Things");
     }
 
     public void firstEvent() {
         // Begin another event or something like that
-        actionOne.setText("Do Other Stuff");
+        //Image view of person running away.
+        text.setText("You ran like a bitch");
+        actionOne.setText("Go back and kill the beast");
+        actionTwo.setText("Keep running and find shelter");
+        actionOne.setOnClickListener(unused -> thirdEvent());
+        actionTwo.setOnClickListener(unused -> fourthEvent());
     }
 
     public void secondEvent() {
         // Begin an action event or fight or something like that
-        actionTwo.setText("Do other stuff again");
+        //Image of person standing with a sword.
+        text.setText("You stayed and fought the beast");
+        actionOne.setText("Eat the guts of the beast");
+        actionTwo.setText("Move on to the next village");
+        actionOne.setOnClickListener(unused -> fifthEvent());
+        actionTwo.setOnClickListener(unused -> sixthEvent());
     }
 
     public void thirdEvent() {
-
+        text.setText("You went back for the beast. Kill him with either a sword or a knife");
+        actionOne.setText("Kill with a sword");
+        actionTwo.setText("Kill with a knife");
+        actionOne.setOnClickListener(unused -> seventhEvent());
+        actionTwo.setOnClickListener(unused -> eigthEvent());
     }
 
     public void fourthEvent() {
+        text.setText("You have found shelter in an abandoned house. You hear a noise upstairs.");
+        actionOne.setText("Investigate upstairs.");
+        actionTwo.setText("Leave the house and keep searching for your next mission");
+        actionOne.setOnClickListener(unused -> ninthEvent());
+        actionTwo.setOnClickListener(unused -> tenthEvent());
+    }
+
+    public void fifthEvent() {
+        text.setText("That's disgusting. You die from poisoning.");
+
+    }
+
+    public void sixthEvent() {
+
+    }
+
+    public void seventhEvent() {
+
+    }
+
+    public void eigthEvent() {
+
+    }
+
+    public void ninthEvent() {
+
+    }
+
+    public void tenthEvent() {
 
     }
 }
