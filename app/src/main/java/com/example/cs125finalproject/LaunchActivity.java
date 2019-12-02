90832348c5d7a514acdaeabf52715fce7a11df50
nicscharpman <ns23@illinois.edu>
2019-12-02 11:48:10 -0600

    Created a launch activity.

--- app/src/main/java/com/example/cs125finalproject/LaunchActivity.java
+++ app/src/main/java/com/example/cs125finalproject/LaunchActivity.java
@@ -1,7 +1,17 @@
 package com.example.cs125finalproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LaunchActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_activity);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
